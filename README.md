a7252a2243e809a5d60e95ca634a10bab385f040
Li Wen Shan <liwenshan@yunzhisheng.cn>
2016-07-02 13:34:33 +0800

    修改模板, 增加批量生成配置文件功能

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-cloud-config files for the coreos machines inside the company.
+__cloud-config files for the coreos machines inside the company.__
+
+### 修改配置文件
+
+```shell
+# 添加一台机器配置
+vim build_config.yml
+---
+10.10.10.191:
+  MAC: "00:e0:81:ee:82:c4"
+  etcd2: False
+  hostname: "coreos-191"
+  nic_name: "enp11s0f0"
+```
+
+### 修改模板
+
+其中有些模板语言, 如: `"{{ hostname }}"`、`{% if etcd2 %}` 等, 这些不能修改, 其他的固化的配置可以修改, 然后统一生效.
+
+```shell
+vim cloud-config.template
+---
+```
+
+### 执行脚本
+
+```shell
+./batch_build_cloud_configs.py
+```
+
+ ![2016-07-02_13-26-16](./img/2016-07-02_13-26-16.png)
---
+++ batch_build_cloud_configs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Batch build cloud-configs
+
+This program is used to batch build multiple CoreOS
+initialized with the cloud-configs configuration file.
+Because differentiation exists for each cloud-config identity.
+Uses pyyaml and jinja2 two extension modules.
+
+@Create on : 16/7/2 10:55
+@Author    : Li Wen Shan
+@version   : 0.1
+"""
+
+from yaml import load
+from jinja2 import Environment, FileSystemLoader
+import os
+import logging
+
+# Setting logger
+logging.basicConfig(format='[%(asctime)s](%(levelname)s)%(name)s: %(message)s', level=logging.INFO)
+logger = logging.getLogger('builder')
+
+# Read the configuration file
+logger.info("Read the configuration file...")
+if not os.path.isfile('build_config.yml'):
+    logger.error('build_config.yml file not found!')
+
+with open('build_config.yml', 'r') as f:
+    b = f.read()
+    build_config = load(b)
+
+# Start batch create
+for k, v in build_config.iteritems():
+    own_ip = k
+    mac_address = v['MAC']
+    hostname = v['hostname']
+    etcd2 = v['etcd2']
+    nic_name = v['nic_name']
+    logger.info("Write file %s.yml ..." % mac_address)
+    env = Environment(loader=FileSystemLoader('./'))
+    body = env.get_template('cloud-config.template').render(own_ip=own_ip,
+                                                            mac_address=mac_address,
+                                                            hostname=hostname,
+                                                            etcd2=etcd2,
+                                                            nic_name=nic_name)
+    with open("%s.yml" % mac_address, "w") as f:
+        f.write(body)
+
+logger.info("Batch create configuration file is completed.")
